fb3ba26a07334eb61d9f1cc343284eb8e0bac1ac
Matthew VanCompernolle <mvancomp@gmail.com>
2014-10-07 14:25:33 -0700

    progress

--- static/assets/js/ncb/mainController.js
+++ static/assets/js/ncb/mainController.js
@@ -1,4 +1,4 @@
-var ncbApp = angular.module('ncbApp', ['snap']);
+var ncbApp = angular.module('ncbApp', ['snap', 'colorpicker.module', 'mgcrea.ngStrap', 'mgcrea.ngStrap.tooltip']);
 
 // disable right drawer
 ncbApp.config(function(snapRemoteProvider) {
@@ -13,10 +13,16 @@ ncbApp.config(function(snapRemoteProvider) {
 });
 
 
-ncbApp.controller("DrawerController", function($scope){
+ncbApp.controller("DrawerController", function(){
 	this.tab = 0;
-	this.localModels = ["Cell 1", "Cell 2", "Cell 3"];
-	this.dbModels = ["Cell 4", "Cell 5", "Cell 6"];
+	this.localModels = [{name: 'Cell 1', classification:'cell'} ,{name: 'Cell Group 2', classification:'cellGroup'}, {name: 'Model 1', classification:'model'}, {name: 'Cell 3', classification:'cell'}];
+	this.dbModels = [{name: 'Cell 4', classification:'cell'} ,{name: 'Cell Group 5', classification:'cellGroup'}, {name: 'Model 3', classification:'model'}, {name: 'Cell 6', classification:'cell'}];
+	this.colors = {cell: '#8781BD' , cellGroup: '#00568C', model:'#5D6B74'};
+
+	this.colorPickerPopover = {
+  		"title": "Title",
+  		"content": "Content"
+	};
 
 	this.selectTab = function(setTab){
 		this.tab = setTab;
@@ -25,4 +31,41 @@ ncbApp.controller("DrawerController", function($scope){
 	this.isSelected = function(checkTab){
 		return this.tab === checkTab;
 	}
+
+	
+	this.styleElement = function(model){
+
+		// style element based off type (cell, cell group, model)
+		if (model.classification === 'cell'){
+			return {
+
+
+                    'background-image': 'linear-gradient(left, '+this.colors.cell+', '+this.colors.cell+' 5%, transparent 5%, transparent 100%)',
+                    'background-image': '-webkit-linear-gradient(left, '+this.colors.cell+', '+this.colors.cell+' 5%, transparent 5%, transparent 100%)',
+                    'color': this.colors.cell
+
+            };
+		}
+		else if (model.classification === 'cellGroup'){
+			return {
+
+                    'background-image': 'linear-gradient(left, '+this.colors.cellGroup+', '+this.colors.cellGroup+' 5%, transparent 5%, transparent 100%)',
+                    'background-image': '-webkit-linear-gradient(left, '+this.colors.cellGroup+', '+this.colors.cellGroup+' 5%, transparent 5%, transparent 100%)',
+                    'color': this.colors.cellGroup
+
+            };
+		}
+		else if (model.classification === 'model'){
+			return {
+
+                    'background-image': 'linear-gradient(left, '+this.colors.model+', '+this.colors.model+' 5%, transparent 5%, transparent 100%)',
+                    'background-image': '-webkit-linear-gradient(left, '+this.colors.model+', '+this.colors.model+' 5%, transparent 5%, transparent 100%)',
+                    'color': this.colors.model
+
+            };
+		}
+	}
+
+	
 });
+
